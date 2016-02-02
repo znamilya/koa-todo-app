990608e2e0708dad350ff721ebcb210b6d605b58
Ilya Znamensky <znamilya@yandex.ru>
2016-02-02 15:33:36 +0300

    add config

---
+++ config/default.js
@@ -0,0 +1,7 @@
+const path = require('path');
+
+
+module.exports = {
+    secret: 'secret_phrase',
+    root: process.cwd(),
+};
--- src/backend/index.js
+++ src/backend/index.js
@@ -1,12 +1,13 @@
 'use strict';
 
 let koa    = require('koa');
+let config = require('config');
 
 let router = require('./routers');
 
 let app    = koa();
 
-
+app.keys = [config.get('secret')];
 app.use(router.routes());
 
 
